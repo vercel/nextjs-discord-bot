94776c2509022100ee3335e570153a6cc0ed4d5d
Rafael Almeida <rafaelalmeidatk@gmail.com>
2021-10-25 14:44:42 -0300

    Antispam: send DM on spam messages

--- src/features/antispam.ts
+++ src/features/antispam.ts
@@ -12,13 +12,14 @@ const MAX_EMOJI_COUNT = 8; // Flags are two symbols so 8 max emojis = max of 4 f
 const emojiRegex = /\p{Emoji_Presentation}/gu;
 const spamKeywords = ['discord', 'nitro', 'steam', 'free', 'gift'];
 
+const dmMessage =
+  'Hello there! Our automated systems detected your message as a spam message and it has been deleted. If this is an error on our side, please feel free to contact one of the moderators.';
+
 export const onMessage: OnMessageHandler = async (client, message) => {
   const emojisCount = message.content.match(emojiRegex)?.length ?? 0;
 
   if (emojisCount > MAX_EMOJI_COUNT) {
-    await message.author.send(
-      'Hello there! Our automated systems detected your message as a spam message and it has been deleted. If this is an error on our side, please feel free to contact one of the moderators.'
-    );
+    await message.author.send(dmMessage);
     await logAndDelete(client, message, 'Emoji spam');
     return;
   }
@@ -31,6 +32,7 @@ export const onMessage: OnMessageHandler = async (client, message) => {
     .some((word) => spamKeywords.includes(word.toLowerCase()));
 
   if (messageHasPingKeywords && messageHasSpamKeywords) {
+    await message.author.send(dmMessage);
     await logAndDelete(client, message, 'Spam keywords');
   }
 };
