6d7ba51f4f47663016ecc4b14039714ca85c61a3
Rafael Almeida <rafaelalmeidatk@gmail.com>
2021-10-26 04:21:50 -0300

    feat: add countdown feature

--- src/utils.ts
+++ src/utils.ts
@@ -46,3 +46,36 @@ export const logAndDelete = async (
 
   message.delete();
 };
+
+type RemainingTime = {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
+
+export const remainingTime = (
+  startTime: number,
+  endTime: number
+): RemainingTime => {
+  // https://stackoverflow.com/a/13904120
+  // get total seconds between the times
+  let delta = Math.abs(endTime - startTime) / 1000;
+
+  // calculate (and subtract) whole days
+  const days = Math.floor(delta / 86400);
+  delta -= days * 86400;
+
+  // calculate (and subtract) whole hours
+  const hours = Math.floor(delta / 3600) % 24;
+  delta -= hours * 3600;
+
+  // calculate (and subtract) whole minutes
+  const minutes = Math.floor(delta / 60) % 60;
+  delta -= minutes * 60;
+
+  // what's left is seconds
+  const seconds = delta % 60; // in theory the modulus is not required
+
+  return { days, hours, minutes, seconds };
+};
